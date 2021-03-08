e0385ae59ba2f39e5306313013a92b6af2d64d8b
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-08 15:22:32 +0530

    Update BatteryMeasure.cs

--- BatteryMeasure.cs
+++ BatteryMeasure.cs
@@ -19,21 +19,27 @@ namespace BatteryManagement
         public static void EvaluateBatteryMeasure(BatteryMeasureFactors battery)
         {
             if(battery.MeasureValue > battery.MaximumLimit)
-                PrintMaximumLimitMessage(battery.MeasureName, battery.MaximumLimit);
+                PrintMaximumLimitMessage(battery.MeasureName, battery.MaximumLimit, battery.MessageLanguage);
             if (battery.MeasureValue < battery.MinimumLimit)
-                PrintMinimumLimitMessage(battery.MeasureName, battery.MinimumLimit);
+                PrintMinimumLimitMessage(battery.MeasureName, battery.MinimumLimit, battery.MessageLanguage);
 
             DisplayOutOfRangeMessage(battery.MeasureName);
         }
-        
-        static void PrintMaximumLimitMessage(string Measure, float MaximumLimit)
+
+        static void PrintMaximumLimitMessage(string Measure, float MaximumLimit, string MessageLanguage)
         {
-            Console.WriteLine(Measure + " has exceeded its Maximum Limit of " + MaximumLimit);
+            if(MessageLanguage == "English")
+                Console.WriteLine(Measure + " has exceeded its Maximum Limit of " + MaximumLimit);
+            if(MessageLanguage == "German")
+                Console.WriteLine("Die " + Measure +" hat seine Höchstgrenze von "+MaximumLimit+" überschritten");
         }
 
-        static void PrintMinimumLimitMessage(string Measure, float MaximumLimit)
+        static void PrintMinimumLimitMessage(string Measure, float MinimumLimit, string MessageLanguage)
         {
-            Console.WriteLine(Measure + " has fall behind its Minimum Limit of " + MaximumLimit);
+            if (MessageLanguage == "English")
+                Console.WriteLine(Measure + " has fall behind its Minimum Limit of " + MinimumLimit);
+            if (MessageLanguage == "German")
+                Console.WriteLine("Die " + Measure+" ist unter seine Mindestgrenze von " + MinimumLimit + " gefallen");
         }
 
         static void DisplayOutOfRangeMessage(string Measure)
@@ -45,13 +51,14 @@ namespace BatteryManagement
     class BatteryMeasureFactors
     {
        public float MeasureValue, MaximumLimit, MinimumLimit;
-       public string MeasureName;
-        public BatteryMeasureFactors(string Name, float Value, float MaximumValue, float MinimumValue)
+       public string MeasureName, MessageLanguage;
+        public BatteryMeasureFactors(string Name, float Value, float MaximumValue, float MinimumValue, string Language)
         {
             this.MeasureName = Name;
             this.MeasureValue = Value;
             this.MaximumLimit = MaximumValue;
             this.MinimumLimit = MinimumValue;
+            this.MessageLanguage = Language;
         }
     }
 }
