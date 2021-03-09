bc0e4434e775b4ed2dbd120f81a6f71300a9a260
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-09 12:27:56 +0530

    Update BatteryLimitMessage.cs

--- BatteryLimitMessage.cs
+++ BatteryLimitMessage.cs
@@ -8,26 +8,26 @@ namespace BatteryManagement
 {
   class BatteryLimitMessage 
   {
-        static void PrintLowBreachMessage(string Measure, string MessageLanguage)
+        public static void PrintLowBreachMessage(string Measure, string MessageLanguage)
         {
             Console.WriteLine(Measure + " running towards low breach limit");
         }
-        static void PrintHighBreachMessage(string Measure, string MessageLanguage)
+        public static void PrintHighBreachMessage(string Measure, string MessageLanguage)
         {
             Console.WriteLine(Measure + " running towards high breach limit");
         }
 
-        static void PrintMaximumLimitMessage(string Measure, float MaximumLimit, string MessageLanguage)
+        public static void PrintMaximumLimitMessage(string Measure, float MaximumLimit, string MessageLanguage)
         {
                 Console.WriteLine(Measure + " has exceeded its Maximum Limit of " + MaximumLimit);
         }
 
-        static void PrintMinimumLimitMessage(string Measure, float MinimumLimit, string MessageLanguage)
+        public static void PrintMinimumLimitMessage(string Measure, float MinimumLimit, string MessageLanguage)
         {
                 Console.WriteLine(Measure + " has fall behind its Minimum Limit of " + MinimumLimit);
         }
 
-        static void DisplayOutOfRangeMessage(string Measure)
+        public static void DisplayOutOfRangeMessage(string Measure)
         {
             Console.WriteLine(Measure + " is out of range!");
         }
