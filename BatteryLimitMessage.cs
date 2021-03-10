b08819aa8b357b91e94f74e916d571aff318f244
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-10 11:54:29 +0530

    Update BatteryLimitMessage.cs

--- BatteryLimitMessage.cs
+++ BatteryLimitMessage.cs
@@ -8,24 +8,24 @@ namespace BatteryManagement
 {
   class BatteryLimitMessage 
   {
-        public static void PrintLowBreachMessage(string Measure, string MessageLanguage)
+        public static void FormatLowBreachMessage(string Measure, string MessageLanguage)
         {
            string Message = (MessageLanguage == "German") ? "Die "+ Measure +" läuft in Richtung ihrer unteren Bruchgrenze" : Measure + " running towards low breach limit";
            PrintMessage(Message);
         }
-        public static void PrintHighBreachMessage(string Measure, string MessageLanguage)
+        public static void FormatHighBreachMessage(string Measure, string MessageLanguage)
         {
             string Message = (MessageLanguage == "German") ? "Die " + Measure + " läuft in Richtung ihrer oberen Bruchgrenze" : Measure + " running towards high breach limit";
             PrintMessage(Message);
         }
 
-        public static void PrintMaximumLimitMessage(string Measure, float MaximumLimit, string MessageLanguage)
+        public static void FormatMaximumLimitMessage(string Measure, float MaximumLimit, string MessageLanguage)
         {
             string Message = (MessageLanguage == "German") ? "Die " + Measure + " hat seine Höchstgrenze von " + MaximumLimit + " überschritten" : Measure + " has exceeded its Maximum Limit of " + MaximumLimit;
             PrintMessage(Message);
         }
 
-        public static void PrintMinimumLimitMessage(string Measure, float MinimumLimit, string MessageLanguage)
+        public static void FormatMinimumLimitMessage(string Measure, float MinimumLimit, string MessageLanguage)
         {
             string Message = (MessageLanguage == "German") ? "Die " + Measure + " ist unter seine Mindestgrenze von " + MinimumLimit + " gefallen" : Measure + " has fall behind its Minimum Limit of " + MinimumLimit;
             PrintMessage(Message);
