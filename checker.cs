0ed2c409007d0e8af87537508cf548cbf1c20266
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-10 11:51:13 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -94,10 +94,14 @@ namespace BatteryManagement
         static int Main()
         {
             PassedBatteryMeasure(batteryIsOk(new BatteryMeasure(25, 70, 0.7f), "English"));
-            FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(60, 65, 0.6f), "German"));
+            FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(60, 65, 0.6f), "English"));
             FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(-50, 85, 0.0f), "English"));
+            FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(43, 10, 0.9f), "English"));
+            
+            PassedBatteryMeasure(batteryIsOk(new BatteryMeasure(25, 70, 0.7f), "German"));
+            FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(60, 65, 0.6f), "German"));
+            FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(-50, 85, 0.0f), "German"));
             FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(43, 10, 0.9f), "German"));
-            Console.WriteLine("All ok");
             return 0;
         }
     }
