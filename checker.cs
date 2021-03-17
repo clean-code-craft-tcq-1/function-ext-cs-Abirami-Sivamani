ff60e1d40a86c657a4d3e188a0ebf6d8df0d233a
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-17 11:28:30 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -5,100 +5,56 @@ namespace BatteryManagement
 {
     class Checker
     {
-        /// <summary>
-        /// Batteries the is ok.
-        /// </summary>
-        /// <param name="temperature">The temperature.</param>
-        /// <param name="soc">The soc.</param>
-        /// <param name="chargeRate">The charge rate.</param>
-        /// <returns></returns>
-        static bool batteryIsOk(BatteryMeasure measures, string Language)
+
+        static void IsBatteryOkay(BatteryMeasure measures, string Language)
         {
-            bool TemperatureMeasureCheck = CheckTemperature(measures.Temperature, Language);
-            bool ChargeStateMeasureCheck = CheckStateOfCharge(measures.StateOfCharge, Language);
-            bool ChargeRateMeasureCheck = CheckChargeRate(measures.ChargeRate, Language);
+            CheckTemperature(measures.Temperature, Language);
+            CheckStateOfCharge(measures.StateOfCharge, Language);
+            CheckChargeRate(measures.ChargeRate, Language);
             BatteryLimitMessage.PrintConsolidatedReport();
-            return (TemperatureMeasureCheck && ChargeStateMeasureCheck && ChargeRateMeasureCheck);
         }
 
-        /// <summary>
-        /// Checks the temperature.
-        /// </summary>
-        /// <param name="temperature">The temperature.</param>
-        /// <returns></returns>
-        static bool CheckTemperature(float temperature, string Language)
+        static void CheckTemperature(float temperature, string Language)
         {
             BatteryMeasureFactors measures = new BatteryMeasureFactors("Temperature", temperature, 45, 0, Language);
-            BatteryMeasure.CheckLowBreach(measures);
-            BatteryMeasure.CheckHighBreach(measures);
-            if (temperature < 0 || temperature > 45)
-            {
-                BatteryMeasure.EvaluateBatteryMeasure(measures);
-                return false;
-            }
-            return true;
+            CheckReachingBreaches(measures);
+            IsBatteryMeasurenOkay(measures);
         }
 
-        /// <summary>
-        /// Checks the state of charge.
-        /// </summary>
-        /// <param name="soc">The soc.</param>
-        /// <returns></returns>
-        static bool CheckStateOfCharge(float soc, string Language)
+        static void CheckStateOfCharge(float soc, string Language)
         {
             BatteryMeasureFactors measures = new BatteryMeasureFactors("State of Charge", soc, 20, 80, Language);
-            BatteryMeasure.CheckLowBreach(measures);
-            BatteryMeasure.CheckHighBreach(measures);
-            if (soc < 20 || soc > 80)
-            {
-                BatteryMeasure.EvaluateBatteryMeasure(measures);
-                return false;
-            }
-            return true;
+            CheckReachingBreaches(measures);
+            IsBatteryMeasurenOkay(measures);
         }
 
-        /// <summary>
-        /// Checks the charge rate.
-        /// </summary>
-        /// <param name="chargeRate">The charge rate.</param>
-        /// <returns></returns>
-        static bool CheckChargeRate(float chargeRate, string Language)
+        static void CheckChargeRate(float chargeRate, string Language)
         {
             BatteryMeasureFactors measures = new BatteryMeasureFactors("Charge Rate",chargeRate, 0.8f, 0.0f, Language);
-            BatteryMeasure.CheckLowBreach(measures);
-            BatteryMeasure.CheckHighBreach(measures);
-            if (chargeRate > 0.8)
-            {
-                BatteryMeasure.EvaluateBatteryMeasure(measures);
-                return false;
-            }
-            return true;
+            CheckReachingBreaches(measures);
+            IsBatteryMeasurenOkay(measures);
+
         }
 
-        static void PassedBatteryMeasure(bool IsBatteryOk)
+        static void CheckReachingBreaches(BatteryMeasureFactors measures)
         {
-            if (!IsBatteryOk)
-            {
-                Console.WriteLine("Expected true, but got false");
-                Environment.Exit(1);
-            }
+            BatteryMeasure.CheckLowBreach(measures);
+            BatteryMeasure.CheckHighBreach(measures);
         }
-        static void FailedBatteryMeasure(bool IsBatteryOk)
+
+        static bool IsBatteryMeasurenOkay(BatteryMeasureFactors measures)
         {
-            if (IsBatteryOk)
-            {
-                Console.WriteLine("Expected false, but got true");
-                Environment.Exit(1);
-            }
+            if (measures.MeasureValue < measures.MinimumLimit || measures.MeasureValue > measures.MaximumLimit)
+                BatteryMeasure.EvaluateBatteryMeasure(measures);
         }
 
         static int Main()
         {
-            PassedBatteryMeasure(batteryIsOk(new BatteryMeasure(25, 70, 0.7f), "English"));
-            FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(60, 65, 0.6f), "English"));
-            FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(-50, 85, 0.0f), "English"));
-            FailedBatteryMeasure(batteryIsOk(new BatteryMeasure(43, 10, 0.9f), "English"));
-            PassedBatteryMeasure(batteryIsOk(new BatteryMeasure(43, 78, 0.7f), "English"));
+            IsBatteryOkay(new BatteryMeasure(25, 70, 0.7f), "English");
+            IsBatteryOkay(new BatteryMeasure(60, 65, 0.6f), "English");
+            IsBatteryOkay(new BatteryMeasure(-50, 85, 0.0f), "English");
+            IsBatteryOkay(new BatteryMeasure(43, 10, 0.9f), "English");
+            IsBatteryOkay(new BatteryMeasure(43, 78, 0.7f), "English");
             return 0;
         }
     }
