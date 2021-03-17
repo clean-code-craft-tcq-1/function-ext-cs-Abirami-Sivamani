56b0684269222fc2fe6797b848ed802c3c7cfbe3
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-17 09:36:03 +0530

    Update BatteryLimitMessage.cs

--- BatteryLimitMessage.cs
+++ BatteryLimitMessage.cs
@@ -12,44 +12,14 @@ namespace BatteryManagement
         public static List<String> MeasureCrossedMinimum = new List<String>();
         public static List<String> MeasureReachingLow = new List<String>();
         public static List<String> MeasureReachingHigh = new List<String>();
-        public static void FormatLowBreachMessage(string Measure, string MessageLanguage)
-        {
-           string Message = (MessageLanguage == "German") ? "Die "+ Measure +" läuft in Richtung ihrer unteren Bruchgrenze" : Measure + " running towards low breach limit";
-           MeasureReachingLow.Add(Measure); 
-          PrintMessage(Message);
-        }
-        public static void FormatHighBreachMessage(string Measure, string MessageLanguage)
-        {
-            string Message = (MessageLanguage == "German") ? "Die " + Measure + " läuft in Richtung ihrer oberen Bruchgrenze" : Measure + " running towards high breach limit";
-            MeasureReachingHigh.Add(Measure);  
-          PrintMessage(Message);
-        }
-
-        public static void FormatMaximumLimitMessage(string Measure, float MaximumLimit, string MessageLanguage)
-        {
-            string Message = (MessageLanguage == "German") ? "Die " + Measure + " hat seine Höchstgrenze von " + MaximumLimit + " überschritten" : Measure + " has exceeded its Maximum Limit of " + MaximumLimit;
-          MeasureCrossedMaximum.Add(Measure);  
-          PrintMessage(Message);
-        }
-
-        public static void FormatMinimumLimitMessage(string Measure, float MinimumLimit, string MessageLanguage)
-        {
-            string Message = (MessageLanguage == "German") ? "Die " + Measure + " ist unter seine Mindestgrenze von " + MinimumLimit + " gefallen" : Measure + " has fall behind its Minimum Limit of " + MinimumLimit;
-           MeasureCrossedMinimum.Add(Measure);  
-          PrintMessage(Message);
-        }
     
-        public static void PrintMessage(string Message)
-        {
-          Console.WriteLine(Message);
-        }
-    
-    public static void PrintConsolidatedReport(){
-      for(int MaxCount = 0 ; MaxCount < MeasureCrossedMaximum.Count; MaxCount++)
+      public static void PrintConsolidatedReport()
       {
-        Console.WriteLine((MeasureCrossedMaximum.Count > 1) ? "Below are the Battery Measures that exceeds Maximum Limit" : "");      
-        Console.WriteLine((MaxCount+1) + ". " + MeasureCrossedMaximum[MaxCount]);
-      }
+        for(int MaxCount = 0 ; MaxCount < MeasureCrossedMaximum.Count; MaxCount++)
+        {
+          Console.WriteLine((MaxCount == 0) ? "Below are the Battery Measures that exceeds Maximum Limit" : "");      
+          Console.WriteLine((MaxCount+1) + ". " + MeasureCrossedMaximum[MaxCount]);
+        }
     }
   }
 }
