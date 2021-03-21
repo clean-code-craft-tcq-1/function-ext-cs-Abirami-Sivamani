fb656af5f180ce29ee52f2a8c4bff78d68b8859e
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-21 14:12:50 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -8,40 +8,38 @@ namespace BatteryManagement
 
         static void IsBatteryOkay(BatteryMeasure measures, string Language)
         {
-            CheckTemperature(measures.Temperature, Language);
-            CheckStateOfCharge(measures.StateOfCharge, Language);
-            CheckChargeRate(measures.ChargeRate, Language);
+            bool isTemperatureOkay = CheckTemperature(measures.Temperature, Language);
+            bool isStateOfChargeOkay = CheckStateOfCharge(measures.StateOfCharge, Language);
+            bool isChargeRateOkay = CheckChargeRate(measures.ChargeRate, Language);
             if(Language == "English")
                 BatteryLimitMessage.CreateConsolidatedReportEN();
             else
                 BatteryLimitMessage.CreateConsolidatedReportDE();
         }
 
-       static void CheckTemperature(float temperature, string Language)
+       static bool CheckTemperature(float temperature, string Language)
         {
             BatteryMeasureFactors measures = new BatteryMeasureFactors("Temperature", temperature, 45, 0, Language);
-            TestBatteryMeasureState(measures);
+            return TestBatteryMeasureState(measures);
         }
 
-        static void CheckStateOfCharge(float soc, string Language)
+        static bool CheckStateOfCharge(float soc, string Language)
         {
             BatteryMeasureFactors measures = new BatteryMeasureFactors("State of Charge", soc, 80, 20, Language);
-            TestBatteryMeasureState(measures);
+            return TestBatteryMeasureState(measures);
         }
 
-        static void CheckChargeRate(float chargeRate, string Language)
+        static bool CheckChargeRate(float chargeRate, string Language)
         {
             BatteryMeasureFactors measures = new BatteryMeasureFactors("Charge Rate", chargeRate, 0.8f, 0.0f, Language);
-            TestBatteryMeasureState(measures);
-
+            return TestBatteryMeasureState(measures);
         }
 
-        static void TestBatteryMeasureState(BatteryMeasureFactors measures)
+        static bool TestBatteryMeasureState(BatteryMeasureFactors measures)
         {
             BatteryMeasure.ReachingHigh(measures);
-            BatteryMeasure.ReachingLow(measures);
-            BatteryMeasure.CrossedMaximum(measures);
-            BatteryMeasure.CrossedMinimum(measures);
+            BatteryMeasure.ReachingLow(measures);       
+            return BatteryMeasure.CrossedMaximum(measures) && BatteryMeasure.CrossedMinimum(measures);
         }
         
         static void PassedBatteryMeasure(bool IsBatteryOk)
